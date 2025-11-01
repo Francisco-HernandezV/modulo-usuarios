272e593ddaee8a07e6ee2c69118b4783270c40c6
Francisco-HernandezV <20230079@uthh.edu.mx>
2025-10-31 22:35:51 -0600

    Apuntar a render en la api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // Aquí apuntará al backend (cuando lo montemos)
+  baseURL: "https://modulo-usuarios-fpig.onrender.com", // Aquí apuntará al backend (cuando lo montemos)
 });
 
 export default api;
