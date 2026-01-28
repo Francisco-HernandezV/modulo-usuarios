e1459eb17f97eaf0b70bd21bc0072f4832eb0a9b
Francisco-HernandezV <20230079@uthh.edu.mx>
2026-01-28 06:35:58 -0600

    Solo una modificacion

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import './styles/theme.css';
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="402358156411-0tknf2rgpq612q5a4d3hha2mpm26vq12.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import './styles/theme.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
